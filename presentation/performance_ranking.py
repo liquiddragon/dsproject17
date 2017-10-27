8d8fbbcea3529acb33f6084566077e465770cdb7
Arturs Polis <arturs.polis@gmail.com>
2017-10-27 12:03:59 +0300

    Added growth rate in unnormalized and normalized formats for top 20 countries

--- presentation/performance_ranking.py
+++ presentation/performance_ranking.py
@@ -16,9 +16,6 @@ gdp_mean = np.mean(gdp_vals_non_nan)
 low = gdp.loc[2015][gdp.loc[2015] < gdp_mean] # values below the mean
 high = gdp.loc[2015][gdp.loc[2015] >= gdp_mean] # values above the mean
 
-high_income_countries = high.index.values
-low_income_countries = low.index.values
-
 # get country names
 high_income_countries = high.index.values
 low_income_countries = low.index.values
@@ -34,20 +31,45 @@ pred_std = np.std(predictions_all.loc[2030])
 
 # convert predictions to "normalized" values
 
-predictions = (predictions-pred_mean)/pred_std
+predictions_normalized = (predictions-pred_mean)/pred_std
 
 # predictions for high and low income countries correspondingly:
 high_predictions = predictions.loc[high_income_countries]
 low_predictions = predictions.loc[low_income_countries]
 
+high_predictions_normalized = predictions_normalized.loc[high_income_countries]
+low_predictions_normalized = predictions_normalized.loc[low_income_countries]
 
 top_performers_low_income = low_predictions.sort_values(ascending=False)[0:20]
 bottom_performers_low_income = low_predictions.sort_values(ascending=False)[-20:]
 
+top_performers_low_income_normalized = low_predictions_normalized.sort_values(ascending=False)[0:20]
+bottom_performers_low_income_normalized = low_predictions_normalized.sort_values(ascending=False)[-20:]
+
+df_top_low = top_performers_low_income.to_frame()
+df_bottom_low = bottom_performers_low_income.to_frame()
+
+df_top_low['normalized'] = top_performers_low_income_normalized
+df_bottom_low['normalized'] = bottom_performers_low_income_normalized
+
 top_performers_high_income = high_predictions.sort_values(ascending=False)[0:20]
 bottom_performers_high_income = high_predictions.sort_values(ascending=False)[-20:]
+
+top_performers_high_income_normalized = high_predictions_normalized.sort_values(ascending=False)[0:20]
+bottom_performers_high_income_normalized = high_predictions_normalized.sort_values(ascending=False)[-20:]
+
+df_top_high = top_performers_high_income.to_frame()
+df_bottom_high = bottom_performers_high_income.to_frame()
+
+df_top_high['normalized'] = top_performers_high_income_normalized
+df_bottom_high['normalized'] = bottom_performers_high_income_normalized
 
 top_performers_low_income.to_csv('presentation/top_performers_low_income.csv')
 bottom_performers_low_income.to_csv('presentation/bottom_performers_low_income.csv')
 top_performers_high_income.to_csv('presentation/top_performers_high_income.csv')
 bottom_performers_high_income.to_csv('presentation/bottom_performers_high_income.csv')
+
+df_top_low.to_csv('presentation/top_performers_low_income_normalized.csv')
+df_bottom_low.to_csv('presentation/bottom_performers_low_income_normalized.csv')
+df_top_high.to_csv('presentation/top_performers_high_income_normalized.csv')
+df_bottom_high.to_csv('presentation/bottom_performers_high_income_normalized.csv')
